cbf2a1caa62e76a8e1318884d632430d181211d3
GhazaryanYuri <yuranar12@gmail.com>
2025-01-22 16:42:55 +0400

    Adde responsivity to My Wallet page

--- Scripts/script.js
+++ Scripts/script.js
@@ -34,3 +34,17 @@ const cardsHTML = cards.join("");
 myWallet.innerHTML = cardsHTML;
 
 // ----------------------------------------------------------------------------
+
+const navbarBtn = document.querySelector(".navbar-btn");
+const navbarLinks = document.querySelector(".links-inactive");
+const navbarFooter = document.querySelector(".navbar-footer-disabled");
+
+navbarBtn.addEventListener("click", () => {
+  if (navbarLinks.className === "links-inactive") {
+    navbarLinks.className = "links-active";
+    navbarFooter.className = "navbar-footer-enable";
+  } else {
+    navbarLinks.className = "links-inactive";
+    navbarFooter.className = "navbar-footer-disabled";
+  }
+});
