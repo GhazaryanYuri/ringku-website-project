dbdd52caf702283337c4a6c5bcdd5f4108109e45
GhazaryanYuri <yuranar12@gmail.com>
2025-01-23 16:23:38 +0400

    Added responsivity to Recent Transactions page

--- Scripts/recent-transactions.js
+++ Scripts/recent-transactions.js
@@ -25,3 +25,19 @@ recentTransactionsData.forEach((item) => {
                                         } item-inner"> $${item.amount} </div>
                                     </div>`;
 });
+
+// ------------------------------------------------------------------------------------
+
+const navbarBtn = document.querySelector(".navbar-btn");
+const navbarLinks = document.querySelector(".links-inactive");
+const navbarFooter = document.querySelector(".navbar-footer-disabled");
+
+navbarBtn.addEventListener("click", () => {
+  if (navbarLinks.className === "links-inactive") {
+    navbarLinks.className = "links-active";
+    navbarFooter.className = "navbar-footer-enable";
+  } else {
+    navbarLinks.className = "links-inactive";
+    navbarFooter.className = "navbar-footer-disabled";
+  }
+});
